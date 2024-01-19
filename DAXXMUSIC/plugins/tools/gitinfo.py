20a4f4ffd6d54170837f98542f58cbe2a65e75a2
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-01-19 16:09:19 +0530

    Update gitinfo.py

--- DAXXMUSIC/plugins/tools/gitinfo.py
+++ DAXXMUSIC/plugins/tools/gitinfo.py
@@ -59,7 +59,7 @@ async def github(_, message):
 ๏ ғᴏʟʟᴏᴡᴇʀs ➠ {followers}
 ๏ ғᴏʟʟᴏᴡɪɴɢ ➠ {following}
 
-๏ ᴍᴀᴅᴇ ʙʏ ➠ [ʀᴏʏ-ᴇᴅɪᴛx](https://t.me/roy_editx)"""
+๏ ᴍᴀᴅᴇ ʙʏ ➠ [sᴀsʜɪᴋᴀɴᴛ](https://t.me/ABOUT_SASHIKANT)"""
 
             except Exception as e:
                 print(str(e))
